179664eac5f5d08fdf496368a0de383f1cff0e71
Gary Rudolph <GaryRudolph@me.com>
2011-07-08 22:14:12 -0700

    Updated error manager to support creating errors using NSLocalizedString

--- LolayErrorManager.h
+++ LolayErrorManager.h
@@ -10,12 +10,20 @@
 
 - (id) initWithDomain:(NSString*) inDomain;
 					   
+- (void) presentErrorCode:(NSInteger) code;
+- (void) presentErrorCode:(NSInteger) code suffix:(NSString*) suffix;
+- (void) presentErrorCode:(NSInteger) code error:(NSError*) error;
+- (void) presentErrorCode:(NSInteger) code suffix:(NSString*) suffix error:(NSError*) error;
 - (void) presentError:(NSError*) error;
 - (void) presentErrors:(NSArray*) errors;
 - (void) presentError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion;
 - (void) presentError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion error:(NSError*) error;
 - (void) presentError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion title:(NSString*) title;
 - (void) presentError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion title:(NSString*) title error:(NSError*) error;
+- (NSError*) createError:(NSInteger) code;
+- (NSError*) createError:(NSInteger) code suffix:(NSString*) suffix;
+- (NSError*) createError:(NSInteger) code error:(NSError*) error;
+- (NSError*) createError:(NSInteger) code suffix:(NSString*) suffix error:(NSError*) error;
 - (NSError*) createError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion;
 - (NSError*) createError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion error:(NSError*) error;
 - (NSError*) createError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion title:(NSString*) title;
--- LolayErrorManager.m
+++ LolayErrorManager.m
@@ -34,6 +34,22 @@
 	[super dealloc];
 }
 
+- (void) presentErrorCode:(NSInteger) code {
+	[self presentErrorCode:code suffix:nil error:nil];
+}
+
+- (void) presentErrorCode:(NSInteger) code suffix:(NSString*) suffix {
+	[self presentErrorCode:code suffix:suffix error:nil];
+}
+
+- (void) presentErrorCode:(NSInteger) code error:(NSError*) error {
+	[self presentErrorCode:code suffix:nil error:error];
+}
+
+- (void) presentErrorCode:(NSInteger) code suffix:(NSString*) suffix error:(NSError*) error {
+	[self presentError:[self createError:code suffix:suffix error:error]];
+}
+
 - (void) presentError:(NSError*) error {
 	DLog(@"enter error=%@", error);
 	if (error == nil) {
@@ -90,6 +106,59 @@
 	[self presentError:[self createError:code description:description recoverySuggestion:recoverySuggestion title:title error:error]];
 }
 
+- (NSError*) createError:(NSInteger) code {
+	return [self createError:code error:nil];
+}
+
+- (NSError*) createError:(NSInteger) code suffix:(NSString*) suffix {
+	return [self createError:code suffix:suffix error:nil];
+}
+
+- (NSError*) createError:(NSInteger) code error:(NSError*) error {
+	return [self createError:code suffix:nil error:error];
+}
+
+- (NSError*) createError:(NSInteger) code suffix:(NSString*) suffix error:(NSError*) error {
+	NSString* titleKey = [NSString stringWithFormat:@"error-%i-localizedTitle", code];
+	NSString* descriptionKey = [NSString stringWithFormat:@"error-%i-localizedDescription", code];
+	NSString* recoveryKey = [NSString stringWithFormat:@"error-%i-recoverySuggestion", code];
+	if (suffix) {
+		titleKey = [titleKey stringByAppendingFormat:@"-%@", suffix];
+		descriptionKey = [descriptionKey stringByAppendingFormat:@"-%@", suffix];
+		recoveryKey = [recoveryKey stringByAppendingFormat:@"-%@", suffix];
+	}
+	
+	NSString* title = NSLocalizedString(titleKey, @"Error title");
+	NSString* description = NSLocalizedString(descriptionKey, @"Error Description");
+	NSString* recoverySuggestion = NSLocalizedString(recoveryKey, @"Error Recovery Suggestion");
+	
+	if ([title length] == 0 || [title isEqualToString:titleKey]) {
+		title = NSLocalizedString(@"error-localizedTitle", @"Error Default Title");
+		
+		if ([title length] == 0 || [title isEqualToString:@"error-localizedTitle"]) {
+			title = nil;
+		}
+	}
+	
+	if ([description length] == 0 || [description isEqualToString:descriptionKey]) {
+		description = NSLocalizedString(@"error-localizedDescription", @"Error Default Description");
+		
+		if ([description length] == 0 || [description isEqualToString:@"error-localizedDescription"]) {
+			description = nil;
+		}
+	}
+	
+	if ([recoverySuggestion length] == 0 || [recoverySuggestion isEqualToString:recoveryKey]) {
+		recoverySuggestion = NSLocalizedString(@"error-recoverySuggestion", @"Error Default Recovery Suggestion");
+		
+		if ([recoverySuggestion length] == 0 || [recoverySuggestion isEqualToString:@"error-recoverySuggestion"]) {
+			recoverySuggestion = nil;
+		}
+	}
+	
+	return [self createError:code description:description recoverySuggestion:recoverySuggestion title:title error:error];
+}
+
 - (NSError*) createError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion {
 	return [self createError:code description:description recoverySuggestion:recoverySuggestion title:nil error:nil];
 }
@@ -103,7 +172,7 @@
 }
 
 - (NSError*) createError:(NSInteger) code description:(NSString*) description recoverySuggestion:(NSString*) recoverySuggestion title:(NSString*) title error:(NSError*) error {
-	NSMutableDictionary* userInfo = [NSMutableDictionary new];
+	NSMutableDictionary* userInfo = [NSMutableDictionary dictionaryWithCapacity:4];
 	if (description) {
 		[userInfo setObject:description forKey:NSLocalizedDescriptionKey];
 	}
@@ -117,7 +186,7 @@
 		[userInfo setObject:error forKey:NSUnderlyingErrorKey];
 	}
 	
-	return [NSError errorWithDomain:self.domain code:code userInfo:[userInfo autorelease]];
+	return [NSError errorWithDomain:self.domain code:code userInfo:userInfo];
 }
 
 #pragma mark -
